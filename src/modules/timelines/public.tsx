908a3b21ae053f9e539829d6235fc0be8112e5c2
Thiago Jedi <thiagoa7@gmail.com>
2023-12-25 15:33:57 -0300

    feat(timelines): temporarily filtering out media posts

--- src/modules/timelines/public.tsx
+++ src/modules/timelines/public.tsx
@@ -6,9 +6,14 @@ export const PublicTimeline = () => {
 
   return (
     <>
-      {statusList.map((status) => (
-        <Post key={status.id} status={status} />
-      ))}
+      {statusList
+        .filter((status) => status.media_attachments.length === 0) // Remove when handling media
+        .map((status) => (
+          <>
+            <Post key={status.id} status={status} />
+            <hr />
+          </>
+        ))}
     </>
   );
 };
