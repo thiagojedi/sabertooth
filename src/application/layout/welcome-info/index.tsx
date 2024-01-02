703a391dc9e942f0257084acb8036542dded350f
Thiago 'Jedi' Cerqueira <thiago.cerqueira@agilecontent.com>
2024-01-02 11:04:48 -0300

    fix: logo rendering on non root deployments

--- src/application/layout/welcome-info/index.tsx
+++ src/application/layout/welcome-info/index.tsx
@@ -2,9 +2,11 @@ import { FunctionalComponent } from "preact";
 
 import styles from "./styles.module.css";
 
+import logo from "/logo.svg?url";
+
 export const WelcomeInfo: FunctionalComponent = () => (
   <div className={styles.welcome}>
-    <img className={styles.logo} src="/logo.svg?url" alt="SaberTooth" />
+    <img className={styles.logo} src={logo} alt="SaberTooth" />
     <p>A small Mastodon application for very small screens</p>
   </div>
 );
