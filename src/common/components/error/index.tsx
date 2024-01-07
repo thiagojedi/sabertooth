525f3b0cf09708cd929d307063d9f9f37ca3887c
Thiago Jedi <thiagoa7@gmail.com>
2024-01-07 11:57:14 -0300

    feat: prevent debug log to show on prod builds

--- src/common/components/error/index.tsx
+++ src/common/components/error/index.tsx
@@ -1,5 +1,12 @@
-export const DebugLog = (props: { info?: object }) => (
-  <pre>
-    <code>{JSON.stringify(props.info, undefined, 1)}</code>
-  </pre>
-);
+import { FunctionalComponent } from "preact";
+
+export const DebugLog: FunctionalComponent<{ info?: object }> = (props) => {
+  if (!import.meta.env.VITE_LOGS) {
+    return null;
+  }
+  return (
+    <pre>
+      <code>{JSON.stringify(props.info, undefined, 1)}</code>
+    </pre>
+  );
+};
