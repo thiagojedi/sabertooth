b21a822063afa9b881c935bcc8efb2c842ffb89f
Thiago Jedi <thiagoa7@gmail.com>
2024-01-28 18:15:18 -0300

    fix(post): not replacing duplicate emojis

--- src/common/helpers/emoji-text.ts
+++ src/common/helpers/emoji-text.ts
@@ -2,7 +2,7 @@ export const emojiText = (text: string, emojis: Emoji[]) => {
   let formattedText = text;
   emojis.forEach((e) => {
     formattedText = formattedText.replace(
-      `:${e.shortcode}:`,
+      new RegExp(`:${e.shortcode}:`, "g"),
       `<img src="${e.static_url}" alt="${e.shortcode}" class="emoji" />`,
     );
   });
